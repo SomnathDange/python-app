b1cd5fca9377e952c1b9afe062a3ab904606b86d
Somanath Dange <somanathdange@Somanaths-MacBook-Air.local>
2026-03-10 13:59:57 +0530

    Commit SHA

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def info():
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
         'message': 'You are doing great, little human! Som Don',
-        'deployed_on': 'kubernetes'
+        'deployed_on': 'kubernetes Backstage'
     })
 
 @app.route('/api/v1/healthz')
